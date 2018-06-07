ec530de0f94c33668f8ac8426f79683e76bfec89
xiaozhen <626269256@qq.com>
2018-06-07 13:32:07 +0800

    模板页

---
+++ README.md
@@ -0,0 +1,14 @@
+### 技术栈：NodeJS+expresese+MongoDB
+
+[![GitHub issues][issues-image]][issues-url]
+[![GitHub stars][stars-image]][stars-url]
+[![GitHub forks][forks-image]][forks-url]
+
+[issues-url]: https://github.com/dengxiaozhen/hulifu-crack/issues
+[issues-image]: https://img.shields.io/github/issues/dengxiaozhen/github-hans.svg
+
+[stars-url]: https://github.com/dengxiaozhen/hulifu-crack/stargazers
+[stars-image]: https://img.shields.io/github/stars/dengxiaozhen/github-hans.svg
+
+[forks-url]: https://github.com/dengxiaozhen/hulifu-crack/network
+[forks-image]: https://img.shields.io/github/forks/dengxiaozhen/github-hans.svg
--- app.js
+++ app.js
@@ -2,7 +2,37 @@
 
 // 加载 express 模块
 const express = require('express');
-// 创建 app 应用
+// 加载模板处理模块，后端逻辑和页面表现分离
+const swig = require('swig');
+// 创建 app 应用，等同于 NodeJS Http.createServer()对象
 const app = express();
+
+/** 
+ * 配置应用模板
+ */
+// 定义当前应用所使用的模板引擎
+// 第一个参数表示模板引擎名称，同时也是模板文件的后缀。第二个参数表示用于处理模板内容的方法
+app.engine('html', swig.renderFile);
+// 设置模板文件存放的目录，第一个参数必须是 views，第二个参数是目录
+app.set('views', './views');
+// 注册所使用的模板引擎，第一个参数必须是 view engine，第二个参数和 app.engine 方法中定义的模板引擎的名称(第一个参数)必须一致
+app.set('view engine', 'html')
+// 在开发过程中，需要关掉模板缓存
+swig.setDefaults({cache: false});
+/**
+ * 首页：
+ *  req request 对象
+ *  res response 对象
+ *  next 函数
+ */
+
+app.get('/', (req, res, next) => {
+    // res.send('<h1>222</h1>');
+    /** 
+     * 第一个参数表示模板文件，相对于 views 目录下，可不写后缀，views/index.html
+     * 第二个参数，传递给模板使用的数据（后期）
+     */
+    res.render('index')
+})
 // 监听 http 请求
 app.listen(8888);
